95bb07f3d02e6f6484f598d6986090b4e1be23c4
DominicArthur <g00407252@atu.ie>
2023-12-11 13:47:51 +0000

    Lab9-Gray Scale

--- Lab9/Lab9.py
+++ Lab9/Lab9.py
@@ -1,1 +1,13 @@
-print("Hello")
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+nrows = 1
+ncols = 1
+
+imgOriginal = cv2.imread('ATU1.jpg')
+imgGray = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2GRAY)
+
+plt.subplot(nrows, ncols, 1), plt.imshow(imgGray, cmap='gray')
+plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
+plt.show()
